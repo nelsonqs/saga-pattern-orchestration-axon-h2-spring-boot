20e8f816deb6c4e3338b788bda30748b20677609
Nelson Quispe <nquispe@gmail.com>
2019-06-06 22:58:23 -0400

    axon config

---
+++ order-service/src/main/java/com/progressivecoder/ordermanagement/orderservice/config/AxonConfig.java
@@ -0,0 +1,17 @@
+package com.progressivecoder.ordermanagement.orderservice.config;
+
+import com.progressivecoder.ordermanagement.orderservice.aggregates.OrderAggregate;
+import org.axonframework.eventsourcing.EventSourcingRepository;
+import org.axonframework.eventsourcing.eventstore.EventStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AxonConfig {
+
+    @Bean
+    EventSourcingRepository<OrderAggregate> orderAggregateEventSourcingRepository(EventStore eventStore){
+        EventSourcingRepository<OrderAggregate> repository = EventSourcingRepository.builder(OrderAggregate.class).eventStore(eventStore).build();
+        return repository;
+    }
+}
