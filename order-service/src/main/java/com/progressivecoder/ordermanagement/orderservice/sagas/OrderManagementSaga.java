a6d3ed8682c857d01cbfdf6aa6daa0bbc3607eb1
Nelson Quispe <nquispe@gmail.com>
2019-06-12 21:14:22 -0400

    axon config

--- order-service/src/main/java/com/progressivecoder/ordermanagement/orderservice/sagas/OrderManagementSaga.java
+++ order-service/src/main/java/com/progressivecoder/ordermanagement/orderservice/sagas/OrderManagementSaga.java
@@ -17,6 +17,8 @@ import org.axonframework.modelling.saga.SagaEventHandler;
 import org.axonframework.modelling.saga.SagaLifecycle;
 import org.axonframework.modelling.saga.StartSaga;
 import org.axonframework.spring.stereotype.Saga;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.util.UUID;
@@ -25,6 +27,7 @@ import java.util.concurrent.TimeoutException;
 
 @Saga
 public class OrderManagementSaga {
+    private static final Logger log = LoggerFactory.getLogger(OrderManagementSaga.class);
 
     @Inject
     private transient CommandGateway commandGateway;
@@ -38,10 +41,9 @@ public class OrderManagementSaga {
     @SagaEventHandler(associationProperty = "orderId")
     public void handle(OrderCreatedEvent orderCreatedEvent) throws TimeoutException {
         String paymentId = UUID.randomUUID().toString();
-        System.out.println("Saga invoked");
-        //associate Saga
+        log.debug(" Start Saga !!!!  ");
+        log.debug(" Start Order with Id ", orderCreatedEvent.orderId);
         SagaLifecycle.associateWith("paymentId", paymentId);
-        System.out.println("order id" + orderCreatedEvent.orderId);
         OrderCreateEntity orderCreateEntity =  orderRepository.findById(orderCreatedEvent.orderId).isPresent() ? orderRepository.findById(orderCreatedEvent.orderId).get() : new OrderCreateEntity();
         if (orderCreateEntity != null) {
             orderCreateEntity.setOrderId( orderCreatedEvent.orderId);
@@ -51,51 +53,76 @@ public class OrderManagementSaga {
             orderCreateEntity.setStatus(Constants.ORDER_CREATED_STATUS);
             orderRepository.save(orderCreateEntity);
         }
-        //send the commands
-        // call rest template
         String paymentMSId = comunicationService.putCommand(paymentId, orderCreatedEvent.orderId, String.valueOf(orderCreatedEvent.price));
-        System.out.println("PaymentMicroServiceId: " + paymentMSId);
+        log.debug("Result to call MicroServices Paymment: ", paymentMSId);
         if (paymentMSId != null && !paymentMSId.isEmpty()) {
+            log.debug("Result OK!!!, to call << Payment >> Micro Service: ", paymentMSId);
             commandGateway.send(new CreateInvoiceCommand(paymentId, orderCreatedEvent.orderId, orderCreatedEvent.itemType));
         } else {
-            SagaLifecycle.removeAssociationWith("paymentId", paymentId);
+            log.error("Result ERROR!!!, to call to <<Paymment>> Micro Service: ", paymentMSId);
+            compensateOrder(orderCreatedEvent.orderId);
         }
     }
+
+    private void compensateOrder(String orderId) {
+       log.debug(" Call to Compensate ORDER, aborted Mission X) ", orderId);
+       SagaLifecycle.removeAssociationWith("orderId", orderId);
+       SagaLifecycle.end();
+    }
+
 
     @SagaEventHandler(associationProperty = "paymentId")
     public void handle(InvoiceCreatedEvent invoiceCreatedEvent) {
         String shippingId = UUID.randomUUID().toString();
-        System.out.println("Saga continued payment");
+        log.debug(" Start Payment with Id ", invoiceCreatedEvent.paymentId);
         //associate Saga with shipping
         SagaLifecycle.associateWith("shipping", shippingId);
-
-        String paymentMSId = comunicationService.putCommandShipping(shippingId, invoiceCreatedEvent.paymentId, invoiceCreatedEvent.orderId,invoiceCreatedEvent.item);
-        System.out.println("PaymentMicroServiceId: " + paymentMSId);
-        if (paymentMSId != null && !paymentMSId.isEmpty()) {
+        String shippingMSId = comunicationService.putCommandShipping(shippingId, invoiceCreatedEvent.paymentId, invoiceCreatedEvent.orderId,invoiceCreatedEvent.item);
+        System.out.println("PaymentMicroServiceId: " + shippingMSId);
+        if (shippingMSId != null && !shippingMSId.isEmpty()) {
+            log.debug("Result OK!!!, to call to << Shipping >>  Micro Service: ", shippingMSId);
             commandGateway.send(new CreateShippingCommand(shippingId, invoiceCreatedEvent.orderId, invoiceCreatedEvent.paymentId));
         } else {
-            SagaLifecycle.removeAssociationWith("paymentId", shippingId);
+            log.error("Result ERROR!!!, to call to << Shipping >> Micro Service: ", shippingMSId);
+            compensatePayment(invoiceCreatedEvent.paymentId,invoiceCreatedEvent.orderId );
         }
+    }
 
-        //send the create shipping command
-
+    private void compensatePayment(String paymentId, String orderId) {
+        log.error(" Call to COMPENSATE PAYMENT, aborted Mission X) order ", orderId, " payment id: ", paymentId);
+        SagaLifecycle.removeAssociationWith("paymentId", paymentId);
+        SagaLifecycle.removeAssociationWith("orderId", orderId);
+        SagaLifecycle.end();
     }
 
     @SagaEventHandler(associationProperty = "orderId")
     public void handle(OrderShippedEvent orderShippedEvent) {
-        System.out.println("Saga continued order");
+        log.debug(" Start Update Order delivered with Id ", orderShippedEvent.orderId);
+        OrderCreateEntity orderCreateEntity =  orderRepository.findById(orderShippedEvent.orderId).isPresent() ? orderRepository.findById(orderShippedEvent.orderId).get() : new OrderCreateEntity();
+        if (orderCreateEntity != null ) {
+            log.debug("Result find order registered ID Order  : ", orderCreateEntity.getOrderId(), " with item ->", orderCreateEntity.getItem());
+            orderCreateEntity.setStatus(Constants.ORDER_DELIVERED_STATUS);
+            orderRepository.save(orderCreateEntity);
+        } else {
+            log.debug("wait momment please : ");
+        }
+//        else {
+//            compensateShipment(orderShippedEvent.orderId,orderShippedEvent.paymentId, orderShippedEvent.shippingId);
+//        }
         commandGateway.send(new UpdateOrderStatusCommand(orderShippedEvent.orderId, String.valueOf(OrderStatus.SHIPPED)));
+    }
+
+    private void compensateShipment(String orderId, String paymentId, String shippingId) {
+        log.error(" Call to COMPENSATE SHIPPMENT, aborted Mission X) order ID", orderId, " payment id: ", paymentId, "shipping id", shippingId);
+        SagaLifecycle.removeAssociationWith("orderId", orderId);
+        SagaLifecycle.removeAssociationWith("paymentId", paymentId);
+        SagaLifecycle.removeAssociationWith("shipping", shippingId);
+        SagaLifecycle.end();
     }
 
     @SagaEventHandler(associationProperty = "orderId")
     public void handle(OrderUpdatedEvent orderUpdatedEvent) {
-        System.out.println("Saga end");
-        OrderCreateEntity orderCreateEntity =  orderRepository.findById(orderUpdatedEvent.orderId).isPresent() ? orderRepository.findById(orderUpdatedEvent.orderId).get() : new OrderCreateEntity();
-        if (orderCreateEntity != null ) {
-            orderCreateEntity.setStatus(Constants.ORDER_DELIVERED_STATUS);
-            orderRepository.save(orderCreateEntity);
-        }
-
+        log.debug(" End Saga, bye  !!! ");
         SagaLifecycle.end();
     }
 }
